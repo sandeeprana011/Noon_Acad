d45c7e5f67447f14e1a57f343892ef41557b12f4
Sandeep Rana <sandeep.rana@investo2o.com>
2019-03-02 13:53:34 +0530

    Initial Commit

---
+++ Noon Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Noon Assignment
+//
+//  Created by Sandeep Rana on 02/03/19.
+//  Copyright © 2019 Sandeep Rana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
